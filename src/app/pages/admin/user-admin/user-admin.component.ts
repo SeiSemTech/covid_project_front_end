d2e85f38c3016ed889f54679623d1e7b10a6b82f
BeymarBurgos <82980012+BeymarBurgos@users.noreply.github.com>
2021-04-28 19:58:28 -0500

    Creacion formulario registro usuarios

--- src/app/pages/admin/user-admin/user-admin.component.ts
+++ src/app/pages/admin/user-admin/user-admin.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
-import {UserFormComponent} from "./user-form/user-form.component";
+import {MatDialog} from '@angular/material/dialog';
+import {UserFormComponent} from './user-form/user-form.component';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AnimationOptions} from 'ngx-lottie';
+import {Router} from '@angular/router';
+import {AuthService} from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-user-admin',
@@ -8,14 +12,23 @@ import {UserFormComponent} from "./user-form/user-form.component";
   styleUrls: ['./user-admin.component.scss']
 })
 export class UserAdminComponent implements OnInit {
+  public options: AnimationOptions = {path: '/assets/lottie/covid_login.json'};
+  public hide = true;
+  public form: FormGroup;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private router: Router, private authService: AuthService, private formBuilder: FormBuilder) { }
 
   testMessage: string = "";
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      user: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      checkbox: [false],
+    });
   }
 
+  // tslint:disable-next-line:typedef
   openDialog(isNew: boolean) {
     const dialogRef = this.dialog.open(UserFormComponent, {
       data: { user_info: 'prueba', is_new: isNew }
@@ -27,5 +40,4 @@ export class UserAdminComponent implements OnInit {
       }
     });
   }
-
 }
