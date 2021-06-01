aac3234c4778a5f80015d1c9865de05448dff395
Sergio Portilla <sergio.portilla@scotiabank.com.co>
2021-06-01 12:38:36 -0500

    solve bug

--- src/app/pages/ministry-health/ministry-health.module.ts
+++ src/app/pages/ministry-health/ministry-health.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BusinessRulesComponent } from './business-rules/business-rules.component';
-import { MinistryHealthRoutingModule } from "./ministry-health.routing";
-import { PopulationVaccineComponent } from './population-vaccine/population-vaccine.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BusinessRulesComponent} from './business-rules/business-rules.component';
+import {MinistryHealthRoutingModule} from "./ministry-health.routing";
+import {PopulationVaccineComponent} from './population-vaccine/population-vaccine.component';
 import {TitleBarModule} from "../../shared/modules/title-bar/title-bar.module";
 import {InfoPanelModule} from "../../shared/modules/info-panel/info-panel.module";
 import {MatCardModule} from '@angular/material/card';
 import {PipesModule} from "../../core/pipes/pipes.module";
-
+import {MatTableModule} from "@angular/material/table";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {SharedModule} from "../../shared/shared.module";
+import {MatInputModule} from "@angular/material/input";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 
 @NgModule({
@@ -18,10 +23,17 @@ import {PipesModule} from "../../core/pipes/pipes.module";
   imports: [
     CommonModule,
     MinistryHealthRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
     TitleBarModule,
     InfoPanelModule,
     MatCardModule,
-    PipesModule
+    PipesModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatIconModule,
+    SharedModule,
+    MatPaginatorModule
   ]
 })
 export class MinistryHealthModule { }
